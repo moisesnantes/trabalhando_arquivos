5bd4cac37c2d47deab89cab1da034486503eaa1e
Moises Nantes <moisesnantes@gmail.com>
2022-09-29 20:11:45 -0400

    Bloco try-with-resources

--- src/application/Programam2.java
+++ src/application/Programam2.java
@@ -9,13 +9,10 @@ public class Programam2 {
 	public static void main(String[] args) {
 		
 		String path = "C:\\Temp\\criando_arquivos\\arquivos\\texto.txt";
-		BufferedReader br = null;
-		FileReader fr = null;
+	
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+					
 			String line = br.readLine();
 			
 			while (line != null) {
@@ -26,16 +23,8 @@ public class Programam2 {
 			}catch (IOException e) {
 				System.out.println("Error: " + e.getMessage());
 			
-			}finally {
-				try {
-					if (br != null)
-						br.close();
-					if (fr != null)
-						fr.close();
-				}catch (IOException e) {
-					e.printStackTrace();
-				}
 			}
 	}
-
 }
+
+
